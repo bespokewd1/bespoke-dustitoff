57efafd94ed54daa8215021fb468bd09776a0a99
Larss830 <larence830@gmail.com>
2024-11-12 02:11:32 +0800

    change navigation layout

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -3,7 +3,7 @@ var CSbody = document.querySelector("body");
 const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
-CShamburgerMenu.addEventListener("click", function () {
+CShamburgerMenu.addEventListener('click', function() {
     CShamburgerMenu.classList.toggle("cs-active");
     CSnavbarMenu.classList.toggle("cs-active");
     CSbody.classList.toggle("cs-open");
@@ -11,14 +11,24 @@ CShamburgerMenu.addEventListener("click", function () {
     ariaExpanded();
 });
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
 function ariaExpanded() {
-    const csUL = document.querySelector("#cs-expanded");
-    const csExpanded = csUL.getAttribute("aria-expanded");
+    const csUL = document.querySelector('#cs-expanded');
+    const csExpanded = csUL.getAttribute('aria-expanded');
 
-    if (csExpanded === "false") {
-        csUL.setAttribute("aria-expanded", "true");
+    if (csExpanded === 'false') {
+        csUL.setAttribute('aria-expanded', 'true');
     } else {
-        csUL.setAttribute("aria-expanded", "false");
+        csUL.setAttribute('aria-expanded', 'false');
     }
 }
+
+// mobile nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+    for (const item of dropDowns) {
+        const onClick = () => {
+        item.classList.toggle('cs-active')
+    }
+    item.addEventListener('click', onClick)
+    }
+                            
